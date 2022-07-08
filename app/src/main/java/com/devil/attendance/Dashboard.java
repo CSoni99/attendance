4af714756ac3d30df606cc1973fa329b6982400e
CSoni9099 <chaitanya02soni@gmail.com>
2022-07-08 10:00:47 +0530

    Initial Commit

--- app/src/main/java/com/devil/attendance/Dashboard.java
+++ app/src/main/java/com/devil/attendance/Dashboard.java
@@ -3,6 +3,10 @@ package com.devil.attendance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Date currentTime = Calendar.getInstance().getTime();
+        TextView dt = findViewById(R.id.Curr_Datetime);
+        dt.setText(currentTime.toString());
     }
 }
